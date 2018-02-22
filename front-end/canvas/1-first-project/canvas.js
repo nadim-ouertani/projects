d2ece2a22ad142f7bd078a62bd16665ccd042457
rafongol <nadimouertani@hotmail.com>
2018-02-22 15:56:28 +0100

    init commit

--- front-end/canvas/1-first-project/canvas.js
+++ front-end/canvas/1-first-project/canvas.js
@@ -51,6 +51,7 @@ function Circle(x, y, dx, dy, radius){
         pen.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         pen.fillStyle = this.color;
         pen.fill();
+        pen.closePath();
     }
     //Update the circle to bounce when reach the eadges
     this.update = function() {
