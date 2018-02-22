3d8f5f0713f55cd9dbc3eddbc21243e544340e61
rafongol <nadimouertani@hotmail.com>
2018-02-22 15:18:27 +0100

    Finishing the script

--- front-end/canvas/1-first-project/canvas.js
+++ front-end/canvas/1-first-project/canvas.js
@@ -13,10 +13,27 @@ var mouse = {
     x: undefined,
     y: undefined
 }
+//Max radius
+var maxRadius = 50;
+//Add a color palette as array
+var colorArray = [
+    "#FFBC67",
+    "#DA727E",
+    "#AC6C82",
+    "#685C79",
+    "#455C7B",
+];
 //Add event listener that trigger onmousemove
 window.addEventListener('mousemove', function(event) {
     mouse.x = event.x;
     mouse.y = event.y;
+})
+//Add event listener that trigger onresize
+window.addEventListener('resize', function() {
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    //Reset our draw
+    init();
 })
 //Create new class circle to create and animate a circle
 function Circle(x, y, dx, dy, radius){
@@ -25,12 +42,14 @@ function Circle(x, y, dx, dy, radius){
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.minRadius = radius;
+    this.color = colorArray[Math.floor(Math.random() * 
+        colorArray.length)]
     //Draw a circle
     this.draw = function(){
         pen.beginPath();
         pen.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        pen.strokeStyle = 'white';
-        pen.stroke();
+        pen.fillStyle = this.color;
         pen.fill();
     }
     //Update the circle to bounce when reach the eadges
@@ -46,8 +65,10 @@ function Circle(x, y, dx, dy, radius){
         //Interactivity
         if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && 
         mouse.y - this.y < 50 && mouse.y - this.y > -50){
-            this.radius += 1;
-        }else if (this.radius > 2){
+            if(this.radius < maxRadius){
+                this.radius += 1;
+            }
+        }else if (this.radius > this.minRadius){
             this.radius -= 1;
         }
         this.draw();
@@ -55,16 +76,18 @@ function Circle(x, y, dx, dy, radius){
 }
 //Create new array of circle
 var circleArray = [];
-for (var i = 0; i < 300 ; i++){
-    var radius = 40;
-    var x = Math.random() * (innerWidth - radius * 2) + radius;
-    var y = Math.random() * (innerHeight - radius *2) + radius;
-    var dx = (Math.random() - 0.5);
-    var dy = (Math.random() - 0.5);
-    //Push circle to each array
-    circleArray.push(new Circle(x, y, dx , dy ,radius));
+function init(){
+    circleArray = [];
+    for (var i = 0; i < 1200 ; i++){
+        var radius = Math.random() * 4 + 1;
+        var x = Math.random() * (innerWidth - radius * 2) + radius;
+        var y = Math.random() * (innerHeight - radius *2) + radius;
+        var dx = (Math.random() - 0.5);
+        var dy = (Math.random() - 0.5);
+        //Push circle to each array
+        circleArray.push(new Circle(x, y, dx , dy ,radius));
+    }
 }
-console.log(circleArray);
 //Animate the circle
 function animate(){
     requestAnimationFrame(animate);
@@ -75,4 +98,5 @@ function animate(){
         circleArray[i].update();
     }
 }
+init();
 animate();
