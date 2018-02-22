9e7863689a4f546d42b320270529e423b96fbe55
rafongol <nadimouertani@hotmail.com>
2018-02-22 12:17:52 +0100

    Add random animation circle

--- front-end/canvas/1-first-project/canvas.js
+++ front-end/canvas/1-first-project/canvas.js
@@ -1,28 +1,59 @@
+//Global declaration
 var canvas = document.querySelector('canvas');
 var pen = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
-
+//Add onresize event listener to resize the canvas with the wondow
 window.addEventListener('resize', function() {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
 })
-
-var x = 200;
-var dx = 6;
-var radius = 40;
+//Create new class circle to create and animate a circle
+function Circle(x, y, dx, dy, radius){
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    //Draw a circle
+    this.draw = function(){
+        pen.beginPath();
+        pen.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        pen.strokeStyle = 'rbg(255,0,0)';
+        pen.stroke();
+    }
+    //Update the circle to bounce when reach the eadges
+    this.update = function() {
+        if (this.x + this.radius > innerWidth || this.x - radius < 0){
+            this.dx = -this.dx;
+        }
+        if (this.y + this.radius > innerHeight || this.y - radius <0){
+            this.dy = -this.dy;
+        }
+        this.x+= this.dx;
+        this.y+= this.dy;
+        this.draw();
+    }
+}
+//Create new circle object
+var circleArray = [];
+for (var i = 0; i < 300 ; i++){
+    var radius = 40;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius *2) + radius;
+    var dx = (Math.random() - 0.5);
+    var dy = (Math.random() - 0.5);
+    circleArray.push(new Circle(x, y, dx , dy ,radius));
+}
+console.log(circleArray);
+//Animate the circle
 function animate(){
     requestAnimationFrame(animate);
-    //Create circle
+    //Clear the sketch for every new circle
     pen.clearRect(0, 0, innerWidth, innerHeight);
-    pen.beginPath();
-    pen.arc(x, 200, radius, 0, Math.PI * 2, false);
-    pen.strokeStyle = 'rbg(255,0,0)';
-    pen.stroke();
-    if (x + radius > innerWidth || x - radius < 0){
-        dx = -dx;
+    //Create circle And animated
+    for (var i = 0; i < circleArray.length; i++){
+        circleArray[i].update();
     }
-    x+= dx;
 }
-
 animate();
