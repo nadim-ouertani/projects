179cdea76dcbb09dea5b1b06d28e484d6bc66143
rafongol <nadimouertani@hotmail.com>
2018-02-22 10:50:07 +0100

    Add animation to circle

--- front-end/canvas/1-first-project/canvas.js
+++ front-end/canvas/1-first-project/canvas.js
@@ -1,4 +1,5 @@
 var canvas = document.querySelector('canvas');
+var pen = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
@@ -6,3 +7,22 @@ window.addEventListener('resize', function() {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
 })
+
+var x = 200;
+var dx = 6;
+var radius = 40;
+function animate(){
+    requestAnimationFrame(animate);
+    //Create circle
+    pen.clearRect(0, 0, innerWidth, innerHeight);
+    pen.beginPath();
+    pen.arc(x, 200, radius, 0, Math.PI * 2, false);
+    pen.strokeStyle = 'rbg(255,0,0)';
+    pen.stroke();
+    if (x + radius > innerWidth || x - radius < 0){
+        dx = -dx;
+    }
+    x+= dx;
+}
+
+animate();
