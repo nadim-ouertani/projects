2248f2d8e1659fc2294a5c82b68111a14e198f9b
rafongol <nadimouertani@hotmail.com>
2018-02-27 17:45:52 +0100

    Add assets

--- vendor/script.js
+++ vendor/script.js
@@ -1,1 +1,32 @@
 //Setup
+var front_img, back_img, fcard, bcard;
+front_img = document.querySelector('#frontimg');
+back_img = document.querySelector('#backimg');
+fcard = document.querySelector('.front');
+bcard = document.querySelector('.back');
+// //Function to zoomin
+// function zoomin(x) {
+//
+// };
+//
+// //Function too zoomout
+// function zoomout(x){
+//     x.style.width = "100%";
+// };
+//
+// //add event listener on hover
+// fcard.addEventListener('mouseover', function () {
+//     zoomin(front_img);
+//     console.log('mouse over Front web card');
+// });
+// bcard.addEventListener('mouseover', function () {
+//     console.log('mouse over Back web card');
+// });
+//
+// //add event listener on out
+// fcard.addEventListener('mouseout', function () {
+//     console.log('mouse out Front web card');
+// });
+// bcard.addEventListener('mouseout', function () {
+//     console.log('mouse out Back web card');
+// });
